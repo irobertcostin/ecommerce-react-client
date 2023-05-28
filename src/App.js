13e2cabe407fd9b97db1ae488df10ebddaa5e4aa
irobertcostin <irobertcostin@gmail.com>
2023-05-28 17:30:13 +0300

    task completed set Cookies for user

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { UserProvider } from "./context/ContextCustomers";
 
@@ -12,44 +12,45 @@ import Products from "./components/Products";
 import Navbar from "./components/Navbar";
 import Product from "./components/Product";
 import { ProductsProvider } from "./context/ContextProducts";
-
+import Cookies from 'js-cookie';
+import { message } from "antd";
+import { ContextUser } from "./context/ContextCustomers";
 
 
 function App() {
 
 
-  let [signedIn, setSignedIn] = useState(true)
-  let [user, setUser] = useState('');
+  let [signedIn, setSignedIn] = useState(false)
+  let [user, setUser] = useState("");
 
 
 
 
   useEffect(() => {
 
-    if (!signedIn) {
-      setUser('');
-    }
 
-  }, [])
+
+  }, [user])
 
 
   return (
 
-    <UserProvider>
-      <ProductsProvider>
-        <BrowserRouter>
-          <Navbar signedIn={signedIn} setSignedIn={setSignedIn} />
-          <Routes>
-            {/* <Route path="/" element={<Hero />}></Route> */}
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/customers/login" element={<Login user={user} setUser={setUser} setSignedIn={setSignedIn} />}></Route>
-            {/* <Route path="/register" element={<Register />}></Route> */}
-            <Route path="/gallery" element={<Products />}></Route>
-            <Route path="/product/:id" element={<Product />}></Route>
-          </Routes>
-        </BrowserRouter>
-      </ProductsProvider>
-    </UserProvider>
+
+
+    <BrowserRouter>
+      <UserProvider>
+        <Navbar signedIn={signedIn} setSignedIn={setSignedIn} />
+        <Routes>
+          {/* <Route path="/" element={<Hero />}></Route> */}
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/customers/login" element={<Login user={user} setUser={setUser} setSignedIn={setSignedIn} />}></Route>
+          {/* <Route path="/register" element={<Register />}></Route> */}
+          <Route path="/gallery" element={<Products />}></Route>
+          <Route path="/product/:id" element={<Product />}></Route>
+        </Routes>
+      </UserProvider>
+    </BrowserRouter>
+
 
   );
 }
